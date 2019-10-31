74830056ec9b520547c55994f315ef8af1fbae27
Pakapol Sanarge <42261963+glasan54@users.noreply.github.com>
2019-10-31 10:10:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,28 +27,7 @@ app.post('/webhook', (req, res,next) => {
   console.log('POST: /');
   console.log('Body: ',req.body);
 
-  //Create an instance
-  const agent = new WebhookClient({
-    request: req,
-    response: res
-  });
-
-  //Test get value of WebhookClient
-  console.log('agentVersion: ' + agent.agentVersion);
-  console.log('intent: ' + agent.intent);
-  console.log('locale: ' + agent.locale);
-  console.log('query: ', agent.query);
-  console.log('session: ', agent.session);
-
-  //Function Location
-  function location(agent) {
-    agent.add('Welcome to Thailand.');
-  }
-
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Location', location);  // "Location" is once Intent Name of Dialogflow Agent
-  agent.handleRequest(intentMap);
+  res.status(201).json(req.body)
 });
 
 app.listen(port, () => {
